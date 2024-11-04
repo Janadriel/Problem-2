5420c1e2fc4b357c771931f8323730e18940a818
Janadriel <pjanadriel@gmail.com>
2024-11-04 18:41:45 +0800

    Create classSubjects.js

---
+++ classSubjects.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Data Structures and Algorithms"
+let classSchedule = "1:00-2:30 in Monday and Friday"
+let classroom = "Computer Laboratory A"
+let classInstructor = "Mr. Vitruvius John Barayuga"
+let studentName = "Jan Adviel T. Pilahan"
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`)
